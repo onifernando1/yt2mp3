1b39ab48bea420e23743045f79eaf67775f78fc4
onifernando1 <onifernando1@gmail.com>
2023-09-11 22:35:19 +0100

    server

---
+++ client/src/components/Download.js
@@ -0,0 +1,17 @@
+const { Readable, Writable, Transform, Duplex } = require("stream");
+
+const Download = () => {
+  const youtubeUrl = "";
+
+  //   useEffect(async () => {
+  //     try {
+  //       const urls = await ytdl(youtubeURL, { quality: "highestaudio" });
+  //       console.log(urls);
+  //     } catch (error) {
+  //       console.error(error);
+  //     }
+  //   }, []);
+  //   return <div></div>;
+};
+
+export default Download;
